871b4f1bfa175ec6b2a730a03e1c2c5b75f1ac20
w4v3 <w4v3@3v4w.com>
2020-04-06 18:46:42 +0800

    init

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM tutum/lamp
 ADD . /app
 ADD mysql-setup.sh /mysql-setup.sh
+RUN apt-get update && apt-get install -y libcurl3 php5-curl
 EXPOSE 80 3306
 CMD ["/run.sh"]
--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $action="";
 foreach ($_REQUEST as $key => $value) {
     $$key = $value;
 }
-$actions=array("login"=>1,"logout"=>1,"website"=>1,""=>1);
+$actions=array("login"=>1,"logout"=>1,"website"=>1,"download"=>1,""=>1);
 if (!array_key_exists($action,$actions)) {
     include($error404_page);
     return;
@@ -56,17 +56,34 @@ if ($action == "login") {
     session_destroy();
     echo "<script language=JavaScript> location.replace(location.origin);</script>";
     return;
+} else if ($action == "download") {
+    $ch = curl_init(); 
+    curl_setopt($ch, CURLOPT_URL, $_GET['fileurl']); 
+    #curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    #curl_setopt($ch, CURLOPT_PROTOCOLS, CURLPROTO_HTTP | CURLPROTO_HTTPS);
+    curl_exec($ch); 
+    curl_close($ch); 
+    return;
 }
 
 $sql="select * from admin where username='".$username."' and password='".$password."'";
 $result = mysqli_query($con,$sql);
 while($row = mysqli_fetch_array($result)) {
-    echo("welcome <a href='".$row['website']."'>".$row['username']."</a>");
+    echo("welcome <a href='".$row['website']."'>".$row['username']."</a> ".$row['website']);
+    echo("<br>");
+    echo("<br>");
     echo('<form>
 website: <input type="text" name="website"><br>
 <input type="submit" value="Modify"><br>
 <input type="hidden" name="action" value="website">
 </form>');
+    echo("<br>");
+    echo('<form>
+    file url: <input type="text" name="fileurl"><br>
+    <input type="submit" value="Download"><br>
+    <input type="hidden" name="action" value="download">
+    </form>');
     echo("<br>");
 }
 
